0266881c9a1d1dadb882074704411c60648c6f2d
Corin <craciuncorin5@gmail.com>
2020-06-15 23:21:29 +0200

    first working file

--- assigment2/blackjack.py
+++ assigment2/blackjack.py
@@ -1,54 +1,33 @@
-from itertools import product
-from timeit import default_timer as timer
-
-start = timer()
-lst = [str(i) for i in range(0, 10)]
+import random
 
 
-def finder(n):
-    numbers = []
-    counter = 0
-    n_digits = 2
-    while n > counter:
-        print('n =', n,'   counter =', counter)
-        tot_perm = list(product(lst, repeat=n_digits))
+deck = [range(0,52)]
+random.shuffle(deck)
 
-        for perm in tot_perm:
-            a = ''
-            for number in perm:
-                a += number
-            b = a[::-1]
-            if a == b or a[0] == '0' or b[0] == '0':
-                continue
+def name(card_idx):
+    # Card indices are numbered 0 - 51. Every fourteenth card is a new suit
+    suits = ["diamonds", "hearts", "clubs", "spades"]
+    cards = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+    "jack", "queen", "king"]
+    isuit = card_idx // 13
+    icard = card_idx % 13
+    txt = cards[icard] + " of " + suits[isuit]
+    return txt
 
-            b = int(a[::-1])
-            a = int(a)
+def value(card_idx):
+    val = card_idx % 13 + 1 if card_idx % 13 < 10 else 10  # your code to determine the card value from its index
+    return val # Value is a number between 1 and 10
 
-            # if a > (int('9'*n_digits)+5)/2:
-            #     break
+def total(hand):
+    tot = sum([value(c) for c in hand])
+    if tot + 10 <= 21:
+        tot += 10
+    # Iterate over the cards in hand, and calculate the total value.
+    return tot # Additional question: What happens if you use the variable name ↪"value" instead of "tot" here?
 
-            add = a+b
-            minus = abs(b - a)
+hand = [0, 3, 5]
 
-            if add % minus == 0 and a not in numbers:
-                numbers.append(a)
-                numbers.append(b)
-                # counter = len(numbers)
-        counter = len(numbers)
-
-        n_digits += 1
-        print('n_digits =', n_digits, '\n')
-
-    return numbers, len(numbers)
-
-
-def sum_dif_rev(n):
-    return numbers_lst[n-1]
-
-
-numbers_lst, counter = finder(65)
-print(sorted(numbers_lst), counter)
-print(sum_dif_rev(3))
-
-end = timer()
-print('time =', end - start, 's')
+# for c in hand:
+#     print(name(c))
+#
+# print(total(hand))
