6b634ca564a793a2cb6b6738cdc19b95a863d582
Corin <craciuncorin5@gmail.com>
2020-06-16 17:53:43 +0200

    added A displays as 1 or 11 depending on hand value

--- assigment2/blackjack.py
+++ assigment2/blackjack.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 deck = [n for n in range(0, 52)]
 random.shuffle(deck)
@@ -31,13 +32,20 @@ def draw_card(deck):
     return card
 
 def player():
-    score = 0
+    cards = []
+    card = draw_card(deck)
+    cards.append(value(card))
     while True:
         card = draw_card(deck)
-        score += value(card)
-        print(f" Your hand is now {score}.")
+        cards.append(value(card))
+        score = sum(cards)
+        alt_score = sum(cards) + 10 if 1 in cards and sum(cards) + 10 < 22 else 0
+        # print(cards)
+        print(f"Your hand is now {score}/{alt_score}." if alt_score else f"Your hand is {score}.")
+        for c in cards:
+            print(name(c-1))
         if score > 21:
-            print(f"You lost! Your hand is {score}.")
+            print(f"You lost! Your hand is {score}/{alt_score}."if alt_score else f"You lost! Your hand is {score}.")
             break
         elif score == 21:
             print("Blackjack!")
@@ -47,40 +55,51 @@ def player():
             if draw_another.lower() == "y":
                 continue
             else:
-                print(f"Your final hand is {score}.")
+                print(f"Your final hand is {score}/{alt_score}." if alt_score else f"Your final hand is {score}.")
                 break
     return score
 
-# counter = 0
-def bank():
+def bank(scores):
     score = 0
+    card = draw_card(deck)
+    score += value(card)
+    print(f"Bank has a {name(card)} ({value(card)}).")
     player_score = player()
     while True:
         card = draw_card(deck)
         score += value(card)
-        if player_score > 21:
-            print(f"Bank hand is {score}.\n")
-            print("You lost!")
-            break
-        elif score > 21:
-            print(f"Bank hand is {score}.\n")
+        print(f"Bank hand is {score}.\n")
+        if score > 21:
+            # print(f"Bank hand is {score}.\n")
             print("You won!")
             break
-        elif score < player_score:
+        elif score < 17:
             continue
-        elif player_score < score < 21:
-            print(f"Bank hand is {score}.\n")
-            print("You lost!")
-            break
+        elif 16 < score < 22:
+            if score > player_score:
+                # print(f"Bank hand is {score}.\n")
+                print("You lost!")
+                break
+            elif score == player_score:
+                print("It's a draw!")
+            elif  score < player_score < 22:
+                # print(f"Bank hand is {score}.\n")
+                print("You won!")
+                break
+            else:
+                break
+
 
 def play():
     nr_players = input("How many players?")
     name_players = []
-    player_chips = [100] * int(nr_players)
+    player_chips = np.full(int(nr_players), 100)
+    player_score = []
     for n in range(int(nr_players)):
         name_players.append(input(f"Player {n+1} name: "))
     for n in range(int(nr_players)):
         print(f"\n{name_players[n]}'s turn! \n")
-        bank()
+        player_score.append(player())
+    bank()
 
-play()
+print(play())
