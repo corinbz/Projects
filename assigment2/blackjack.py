ad011e8a318e18860f4bd820e285ab0342300e27
Corin <craciuncorin5@gmail.com>
2020-06-16 21:41:55 +0200

    1. changed total of cards to 416 2. fixed input exceptions 3. added comments

--- assigment2/blackjack.py
+++ assigment2/blackjack.py
@@ -1,7 +1,7 @@
 import random
 import numpy as np
 
-deck = [n for n in range(0, 52)]
+deck = [n for n in range(0, 416)]
 random.shuffle(deck)
 
 def name(card_idx):
@@ -9,7 +9,7 @@ def name(card_idx):
     suits = ["diamonds", "hearts", "clubs", "spades"]
     cards = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
              "jack", "queen", "king"]
-    isuit = card_idx // 13
+    isuit = card_idx // 13 if card_idx // 13 < 4 else card_idx // 13 % 4
     icard = card_idx % 13
     txt = cards[icard] + " of " + suits[isuit]
     return txt
@@ -25,13 +25,14 @@ def total(hand):
     # Iterate over the cards in hand, and calculate the total value.
     return tot # Additional question: What happens if you use the variable name ↪"value" instead of "tot" here?
 
-
 def draw_card(deck):
+    """Draws a card and removes it from the deck"""
     card = random.choice(deck)
     deck.remove(card)
     return card
 
-def player():
+def player(player):
+    """Function for playing the hand that returns the total score"""
     global result
     cards = []
     card = draw_card(deck)
@@ -47,7 +48,7 @@ def player():
             print(name(c-1))
         if score > 21:
             print(f"You busted! Your hand is {score}.")
-            result.append(f"{name_players[n-1]} vs Bank! \n You lost!\n")
+            result.append(f"{name_players[player]} vs Bank! \n You lost!\n")
             break
         elif score == 21:
             print("Blackjack!")
@@ -61,8 +62,6 @@ def player():
                 break
     return score
 
-
-
 def bank(scores):
     global score
     global card
@@ -71,55 +70,77 @@ def bank(scores):
     running = True
 
     while running:
+        #Draws the second card and adds it's value to the score
         card = draw_card(deck)
-        score += value(card)
-        # print(f"Bank hand is {score}.")
+        bank_cards.append(card)
+        score = total(bank_cards)
+        #When total hand is higher than 21 stop the loop
         if score > 21:
             print("Bank busted!")
+            #if players are not busted, store the result
             for player_score in scores:
                 if player_score < 22:
-                    result.append(f"{name_players[scores.index(player_score)]} vs Bank!\n You won!\n")
+                    result.append(f"{name_players[scores.index(player_score)]} vs Bank!\nYou won!\n")
             running = False
 
-        elif score < 17:
-            continue
-        if 16 < score < 22:
+        if 17 < score < 22 :
+            #bank stops drawing , compares the scores and stores the result
             for player_score in scores:
                 if score > player_score:
-                    result.append(f"{name_players[scores.index(player_score)]} vs Bank!\n You lost!\n")
+                    result.append(f"{name_players[scores.index(player_score)]} vs Bank!\nYou lost!\n")
                     continue
                 elif score == player_score:
                     result.append(f"{name_players[scores.index(player_score)]} vs Bank!\nIt's a draw!\n")
                     continue
                 elif  score < player_score < 22:
-                    result.append(f"{name_players[scores.index(player_score)]} vs Bank!\n You won!\n")
+                    result.append(f"{name_players[scores.index(player_score)]} vs Bank!\nYou won!\n")
                     continue
-            running = False
+        elif score == 17 and 1 in [value(c) for c in bank_cards]:
+            continue
+        else:
+            continue
+        running = False
+
+    #Prints the final result
     print(f"\nBank's hand is {score}.\n")
     for i in result:
         print(i)
 
+def play():
+    global first_bank_c
+    # player_chips = np.full(int(nr_players), 100)
+    players_scores = []
+    print(first_bank_c) #Prints bank's first card
 
+    #Each player plays his hand and add the result to player scroes
+    for n in range(int(nr_players)):
+        print(f"\n{name_players[n]}'s turn! \n")
+        players_scores.append(player(n))
 
-result = []
+    bank(players_scores)
 
+result = [] #Stores strings about result between player and bank
+
+########### Draws a card for the bank and stores it to be printed later #######
 card = draw_card(deck)
-score = value(card)
+bank_cards = []
+bank_cards.append(0)
+score = total(bank_cards)  #The bank's total score
 first_bank_c = (f"Bank has a {name(card)} ({value(card)}).")
 
-nr_players = input("How many players?")
+########## Get the info about players ###########
+while True:
+    nr_players = input("How many players?\n")
+    if nr_players.isdigit() == True and 0 < int(nr_players) <= 7:
+        break
+    else:
+         print("Please enter a number between 1 and 7!\n")
+
 name_players = []
 for n in range(int(nr_players)):
     name_players.append(input(f"Player {n+1} name: "))
 
-def play():
-    global first_bank_c
-    player_chips = np.full(int(nr_players), 100)
-    players_scores = []
-    print(first_bank_c)
-    for n in range(int(nr_players)):
-        print(f"\n{name_players[n]}'s turn! \n")
-        players_scores.append(player())
-    bank(players_scores)
+
+
 
 play()
